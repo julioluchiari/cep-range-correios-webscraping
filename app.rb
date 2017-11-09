6b58a18f0dda1910fccb683ac8e04d739804a624
Júlio Luchiari <julio.luchiari@gmail.com>
2017-11-09 19:37:27 -0200

    feat: add all brazilian ufs

--- app.rb
+++ app.rb
@@ -7,54 +7,62 @@ url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/ResultadoBuscaFaixa
 faraday = Faraday.new(url: url)
 
 form_data = {
-  UF: 'SP',
   Localidade: '**',
   Bairro: '',
-  qtdrow: 50,
-  pagini: 1,
-  pagfim: 50
+  qtdrow: 100
 }
 
-zipcode_array = []
+ufs = %w[AC AL AM AP BA CE DF ES GO MA MG MS MT PA 
+         PB PE PI PR RJ RN RO RR RS SC SE SP TO]
+
+zipcode_hash = {}
 
 content_type = 'application/x-www-form-urlencoded; charset=utf-8'
 
-(1..800).step(100) do |index|
-  form_data[:pagini] = index
-  form_data[:pagfim] = index + 100
+ufs.each do |uf|
+  range_array = []
 
-  response = faraday.post do |req|
-    req.body = form_data
-    req.headers['Content-Type'] = content_type
-  end
+  (1..800).step(100) do |index|
+    form_data[:UF] = uf
+    form_data[:pagini] = index
+    form_data[:pagfim] = index + 100
 
-  selector = 'body > div.back > div.tabs > div:nth-child(2) > div > div > div.column2 > div.content > div.ctrlcontent > table'
-
-  selector += ':nth-child(9)' if index == 1
-
-  page = Nokogiri::HTML(response.body)
-  rows = page.css("#{selector} > tr:nth-child(n+3)")
-  rows.each do |r|
-    values = []
-    cells = r.css('td')
-    cells.each_with_index do |c, i|
-      if i == 1
-        splited = c.text.split('a')
-        values.push splited[0].strip.delete('-')
-        values.push splited[1].strip.delete('-')
-      else
-        values.push c.text.strip
-      end
+    response = faraday.post do |req|
+      req.body = form_data
+      req.headers['Content-Type'] = content_type
     end
 
-    zipcode_array.push(locale: values[0],
-                       zipcode_from: values[1],
-                       zipcode_to: values[2],
-                       situation: values[3],
-                       zipcode_type: values[4])
-  end
-end
+    selector = 'body > div.back > div.tabs > div:nth-child(2) > div > div > div.column2 > div.content > div.ctrlcontent > table'
 
-File.open('zipcode_range.json', 'w') do |f|
-  f.write(JSON.pretty_generate(zipcode_array))
+    selector += ':nth-child(9)' if index == 1
+
+    page = Nokogiri::HTML(response.body)
+    rows = page.css("#{selector} > tr:nth-child(n+3)")
+    rows.each do |r|
+      values = []
+      cells = r.css('td')
+      cells.each_with_index do |c, i|
+        if i == 1
+          splited = c.text.split('a')
+          values.push splited[0].strip.delete('-')
+          values.push splited[1].strip.delete('-')
+        else
+          values.push c.text.strip
+        end
+      end
+
+      range_array.push(locale: values[0],
+                        zipcode_from: values[1],
+                        zipcode_to: values[2],
+                        situation: values[3],
+                        zipcode_type: values[4])
+    end
+  end
+
+
+  zipcode_hash[uf] = range_array
+
+  File.open('zipcode_range.json', 'w') do |f|
+    f.write(JSON.pretty_generate(zipcode_hash))
+  end
 end
